5b75a88fa6507e54c3e1e6b6bd61a3f563e8a45f
dawidkaszuba <dawidk89@gmail.com>
2021-11-27 22:45:13 +0100

    added CircuitBreaker and fallback method

--- src/main/java/pl/dawidkaszuba/expense/controller/ExpenseController.java
+++ src/main/java/pl/dawidkaszuba/expense/controller/ExpenseController.java
@@ -3,6 +3,7 @@ package pl.dawidkaszuba.expense.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.dawidkaszuba.expense.config.ExpenseServiceConfig;
@@ -30,7 +31,9 @@ public class ExpenseController {
     public Expense getExpense(@PathVariable("id")  Long id) {
         return expenseService.findExpenseById(id);
     }
+
     @GetMapping("/user/{id}")
+    @CircuitBreaker(name="userExpenses", fallbackMethod = "userExpensesFallBack")
     public ResponseTemplateUserWithExpenses getUserExpenses(@PathVariable("id") Long id) {
         return expenseService.findExpensesByUserId(id);
     }
@@ -41,5 +44,10 @@ public class ExpenseController {
         Properties properties = new Properties(expenseServiceConfig.getMsg(), expenseServiceConfig.getBuildVersion());
         String jsonStr = ow.writeValueAsString(properties);
         return jsonStr;
+    }
+
+    private ResponseTemplateUserWithExpenses userExpensesFallBack(Long id, Throwable t) {
+       //just example, to refactor
+        return new ResponseTemplateUserWithExpenses();
     }
 }
